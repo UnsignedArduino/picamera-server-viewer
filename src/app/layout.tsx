759cfc853c31f30f9265a40839819746836edeac
UnsignedArduino <UnsignedArduino@outlook.com>
2023-07-11 17:29:56 -0400

    Tooling yummy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'PiCamera Server',
-  description: 'View and control your camera stream in the browser!',
-}
+  title: "PiCamera Server",
+  description: "View and control your camera stream in the browser!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <main>
       <div>Empty!</div>
     </main>
-  )
+  );
 }
